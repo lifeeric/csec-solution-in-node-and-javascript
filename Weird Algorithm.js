6236571cbcdd23b047a2e21744b0e34c4a2523a2
LIFE <dkrshw65@gmail.com>
2020-06-30 13:42:32 -0400

    weird

---
+++ Weird Algorithm.js
@@ -0,0 +1,24 @@
+var readline = require("readline")
+
+var r = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+})
+
+r.on("line", function (line) {
+  var vals = line.split(" ")
+  var input = parseInt(vals[0])
+  let n
+
+  let result = []
+  while (true) {
+    result.push(input)
+    if (input === 1) break
+    if (input % 2 === 0) input = input / 2
+    else input = input * 3 + 1
+  }
+
+  console.log(result.join(" "))
+})
+
